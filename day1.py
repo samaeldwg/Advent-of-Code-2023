7efa1bb5ecc3c5caa38f2a316b0f800e3b28c57e
Raphael Zhang <zhangr9@stconline.edu.hk>
2023-12-01 19:33:00 +0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,43 +1,19 @@
-digit_map = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-}
+with open("day1.txt") as file:
+    data = [i for i in file.read().strip().split("\n")]
+# now we have all the strings in a list called data
+"""
+next loop through data 
+first and last 
+"""
+# part 1
+k = []
+for i in data:
+    nums = [int(j) for j in i if j.isdigit()]
+    f = nums[0]
+    l = nums[-1]
+    k.append(int(str(f) + str(l)))
 
-for i in range(1, 10):
-    key = str(i)
-    digit_map[key] = i
+n = sum(k)
+print(n)
 
-ans = 0
 
-with open("day1.txt", "r") as file:
-    data = [line.strip() for line in file]
-
-for line in data:
-    found_numbers = []
-
-    for key, value in digit_map.items():
-        indices = [index for index in range(len(line)) if line.startswith(key, index)]
-
-        for index in indices:
-            found_numbers.append((index, value))
-
-            last_index = line.rfind(key)
-            if last_index != index:
-                found_numbers.append((last_index, value))
-
-    found_numbers.sort(key=lambda x: x[0])
-
-    first_number = found_numbers[0][1]
-    last_number = found_numbers[-1][1]
-    num = first_number * 10 + last_number
-
-    ans += num
-
-print("Answer:", ans)
