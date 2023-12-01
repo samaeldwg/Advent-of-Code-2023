dc2375d21d3b819ec4b71c24a4899f7c65499990
Raphael Zhang <zhangr9@stconline.edu.hk>
2023-12-01 19:31:31 +0800

    AOC
    Advent of Code Day 1

---
+++ day1.1.py
@@ -0,0 +1,43 @@
+digit_map = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+for i in range(1, 10):
+    key = str(i)
+    digit_map[key] = i
+
+ans = 0
+
+with open("day1.txt", "r") as file:
+    for line in file:
+        found_numbers = []
+
+        for key, value in digit_map.items():
+            indices = [
+                index for index in range(len(line)) if line.startswith(key, index)
+            ]
+
+            for index in indices:
+                found_numbers.append((index, value))
+
+                last_index = line.rfind(key)
+                if last_index != index:
+                    found_numbers.append((last_index, value))
+
+        found_numbers.sort(key=lambda x: x[0])
+
+        first_number = found_numbers[0][1]
+        last_number = found_numbers[-1][1]
+        num = first_number * 10 + last_number
+
+        ans += num
+
+print("Answer:", ans)
--- day1.py
+++ day1.py
@@ -1,9 +1,43 @@
-n = [9,8,7,6,5,4,3,2,1]
-for i in range(len(n)-1):
-    for j in range(len(n)-1):
-        if n[j] > n[j+1]: #9 > 8 
-            temp = n[j] # 9 
-            n[j] = n[j+1]
-            n[j+1] = temp 
-print(n) 
+digit_map = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
 
+for i in range(1, 10):
+    key = str(i)
+    digit_map[key] = i
+
+ans = 0
+
+with open("day1.txt", "r") as file:
+    data = [line.strip() for line in file]
+
+for line in data:
+    found_numbers = []
+
+    for key, value in digit_map.items():
+        indices = [index for index in range(len(line)) if line.startswith(key, index)]
+
+        for index in indices:
+            found_numbers.append((index, value))
+
+            last_index = line.rfind(key)
+            if last_index != index:
+                found_numbers.append((last_index, value))
+
+    found_numbers.sort(key=lambda x: x[0])
+
+    first_number = found_numbers[0][1]
+    last_number = found_numbers[-1][1]
+    num = first_number * 10 + last_number
+
+    ans += num
+
+print("Answer:", ans)
